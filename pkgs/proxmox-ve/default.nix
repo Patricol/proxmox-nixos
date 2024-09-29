531aedcabeaa67d8b578117533a75ca7cb0cc8de
Patrick Collins <PatrickCP0@Gmail.com>
2024-09-28 21:53:56 -0600

    more iteration

--- pkgs/proxmox-ve/default.nix
+++ pkgs/proxmox-ve/default.nix
@@ -21,19 +21,19 @@ buildEnv rec {
   name = "proxmox-ve-${pve-manager.version}";
 
   paths = [
-    pve-access-control
-    pve-cluster
-    pve-container
-    pve-firewall
-    (pve-ha-manager.override { inherit enableLinstor; })
-    (pve-manager.override { inherit enableLinstor; })
-    pve-qemu-server
-    (pve-storage.override { inherit enableLinstor; })
-    termproxy
-    vncterm
+#    pve-access-control
+#    pve-cluster
+#    pve-container
+#    pve-firewall
+#    (pve-ha-manager.override { inherit enableLinstor; })
+#    (pve-manager.override { inherit enableLinstor; })
+#    pve-qemu-server
+#    (pve-storage.override { inherit enableLinstor; })
+#    termproxy
+#    vncterm
     wget
     util-linux
-  ] ++ lib.optionals enableLinstor [ linstor-client ];
+  ];# ++ lib.optionals enableLinstor [ linstor-client ];
 
   meta = with lib; {
     description = "A complete, open-source server management platform for enterprise virtualization";
