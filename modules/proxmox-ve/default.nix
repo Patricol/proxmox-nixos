083cd24b112b10b22dc1078a829cec53f862b6e6
Patrick Collins <PatrickCP0@Gmail.com>
2024-09-28 21:36:13 -0600

    seeing what breaks tailscale

--- modules/proxmox-ve/default.nix
+++ modules/proxmox-ve/default.nix
@@ -16,15 +16,15 @@ in
     camillemndn
   ];
 
-  imports = [
-    ./cluster.nix
-    # ./firewall.nix
-    # ./ha-manager.nix
-    ./linstor.nix
-    ./manager.nix
-    ./rrdcached.nix
-    ./vms.nix
-  ];
+#  imports = [
+#    ./cluster.nix
+#    # ./firewall.nix
+#    # ./ha-manager.nix
+#    ./linstor.nix
+#    ./manager.nix
+#    ./rrdcached.nix
+#    ./vms.nix
+#  ];
 
   options.services.proxmox-ve = {
     enable = mkEnableOption "Proxmox VE";
@@ -34,53 +34,53 @@ in
 
   config = mkIf cfg.enable (mkMerge [
     {
-      boot.supportedFilesystems = [
-        "fuse"
-        "glusterfs"
-      ];
-
-      networking.hosts = {
-        "127.0.0.2" = lib.mkForce [ ];
-        "::1" = lib.mkForce [ ];
-      };
-
-      # create the /etc/network/interfaces file for proxmox
-      systemd.tmpfiles.rules = [
-        "d /etc/network 0755 root root -"
-        "f /etc/network/interfaces 0755 root root -"
-      ];
-
-      services.openssh = {
-        enable = true;
-        settings.AcceptEnv = "LANG LC_*";
-      };
-      programs.ssh.extraConfig = ''
-        SendEnv LANG LC_*
-      '';
-
-      security.pam.services."proxmox-ve-auth" = {
-        logFailures = true;
-        nodelay = true;
-      };
-
-      services.rpcbind.enable = true;
-      services.rrdcached.enable = true;
-
-      users.users.www-data = {
-        isSystemUser = true;
-        group = "www-data";
-      };
-      users.groups.www-data = { };
-
-      environment.systemPackages = [ cfg.package ];
-      environment.etc.issue.enable = false;
-
-      networking.firewall.allowedTCPPorts = [
-        80
-        111
-        443
-        8006
-      ];
+#      boot.supportedFilesystems = [
+#        "fuse"
+#        "glusterfs"
+#      ];
+#
+#      networking.hosts = {
+#        "127.0.0.2" = lib.mkForce [ ];
+#        "::1" = lib.mkForce [ ];
+#      };
+#
+#      # create the /etc/network/interfaces file for proxmox
+#      systemd.tmpfiles.rules = [
+#        "d /etc/network 0755 root root -"
+#        "f /etc/network/interfaces 0755 root root -"
+#      ];
+#
+#      services.openssh = {
+#        enable = true;
+#        settings.AcceptEnv = "LANG LC_*";
+#      };
+#      programs.ssh.extraConfig = ''
+#        SendEnv LANG LC_*
+#      '';
+#
+#      security.pam.services."proxmox-ve-auth" = {
+#        logFailures = true;
+#        nodelay = true;
+#      };
+#
+#      services.rpcbind.enable = true;
+#      services.rrdcached.enable = true;
+#
+#      users.users.www-data = {
+#        isSystemUser = true;
+#        group = "www-data";
+#      };
+#      users.groups.www-data = { };
+#
+#      environment.systemPackages = [ cfg.package ];
+#      environment.etc.issue.enable = false;
+#
+#      networking.firewall.allowedTCPPorts = [
+#        80
+#        111
+#        443
+#        8006
+#      ];
     }
   ]);
 }
